82674d32be35ff6f875f5ced4f7f38e3d4d380c5
Taina Conde <tainabatistaconde@gmail.com>
2021-07-05 18:50:37 -0400

    feat: Layout - create component
    This component will be used across all pages. Create components
    and Layout folder. Build up component with minimal code.

---
+++ components/Layout/index.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledLayout = styled.div``;
+
+
+export default function Layout({ children }) {
+  return <StyledLayout>{children}</StyledLayout>;
+}
+
