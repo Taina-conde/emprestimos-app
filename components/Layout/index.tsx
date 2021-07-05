5a3d31415cd337f3651ff777d46023a17a1068ed
Taina Conde <tainabatistaconde@gmail.com>
2021-07-05 19:55:28 -0400

    feat: Layout - add Head with meta tags

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,9 +1,23 @@
-import styled from 'styled-components';
+import Head from "next/head";
+import styled from "styled-components";
 
 const StyledLayout = styled.div``;
 
-
-export default function Layout({ children } : {children: React.ReactNode}) {
-  return <StyledLayout>{children}</StyledLayout>;
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <StyledLayout>
+      <Head>
+        <meta
+          name="description"
+          content="Solicite empréstimos"
+        />
+        <meta
+          property="og:image"
+          content={}
+        />
+        <meta name="og:title" content="Klutch Tecnologia" />
+      </Head>
+      <main>{children}</main>
+    </StyledLayout>
+  );
 }
-
