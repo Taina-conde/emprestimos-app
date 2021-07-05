72690b6eb961f965c6346a3cff73dd01666d331c
Taina Conde <tainabatistaconde@gmail.com>
2021-07-05 19:24:36 -0400

    style: Create DefaultTheme declaration and appTheme
    Use colors from xd files to set the main and secondary colors of
    the app theme.

---
+++ styles/app-theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from 'styled-components';
+
+const appTheme: DefaultTheme = {
+  colors: {
+    main: '#228A95',
+    secondary: '#EF9C4B',
+    text: {
+        main: '#777777',
+        secondary: '#FFFFFF',
+    }
+  },
+};
+
+export { appTheme };
---
+++ styles/styled.d.ts
@@ -0,0 +1,16 @@
+import 'styled-components';
+
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      main: string;
+      secondary: string;
+      text: {
+          main: string;
+          secondary: string;
+      }
+    
+    };
+  }
+}
