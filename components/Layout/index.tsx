4a177a215d16a3388a5aa28e67c3fcb8b03492d6
Taina Conde <tainabatistaconde@gmail.com>
2021-07-05 21:54:24 -0400

    style: Layout - styled Nav with menu icon and Klutch logo

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,14 +1,22 @@
 import Head from "next/head";
 import Image from "next/image";
 import styled from "styled-components";
-import { IoMdMenu } from 'react-icons/io';
-import klutchLogo from '../../assets/icons/Grupo 290.svg'
+import { IoMdMenu } from "react-icons/io";
+import klutchLogo from "../../assets/icons/Grupo 290.svg";
 const Container = styled.div``;
 
 const Nav = styled.nav`
-background-color: ${props => props.theme.colors.primary.main};
- 
-
+  background-color: ${(props) => props.theme.colors.primary.main};
+  height: 60px;
+  width: 100%;
+  box-shadow: 0px 3px 6px #00000029;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const StyledLogo = styled.div`
+  flex-grow: 2;
+  text-align: center;
 `;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -21,11 +29,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       </Head>
       <main>
         <Nav>
-          <IoMdMenu/>
-          <Image src = {klutchLogo} alt = 'Klutch Tecnologia'/>
+          <IoMdMenu size={40} color="white" />
 
-          
-          
+          <StyledLogo>
+            <Image src={klutchLogo} alt="Klutch Tecnologia" />
+          </StyledLogo>
         </Nav>
         <Container>{children}</Container>
       </main>
