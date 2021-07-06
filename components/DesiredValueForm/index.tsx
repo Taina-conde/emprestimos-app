33b664066da08bb4e06a005e055e59aeffa5cdd3
Taina Conde <tainabatistaconde@gmail.com>
2021-07-06 14:30:45 -0400

    refactor: DesiredValueForm - move the code related to this component from app

--- components/DesiredValueForm/index.tsx
+++ components/DesiredValueForm/index.tsx
@@ -7,45 +7,64 @@ import {
   HelperText,
   Label,
 } from "./styled";
-import { FormikProps } from "formik";
+import { Formik, FormikHelpers, FormikErrors } from "formik";
 
-interface FormValues {
+interface Values {
   desiredValue: number;
 }
 
-export default function DesiredValueForm(
-  props:  FormikProps<FormValues>
-) {
-  const {
-    touched,
-    errors,
-    values,
-    handleChange,
-    handleBlur,
-    handleSubmit,
-  } = props;
+export default function DesiredValueForm() {
   return (
-    <Form onSubmit={handleSubmit}>
-      <Title>Valor desejado</Title>
-      <Wrapper>
-        <Label>
-          <Input
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.desiredValue}
-            border={
-              touched.desiredValue && errors.desiredValue && "1px solid red"
-            }
-            type="text"
-            name="desiredValue"
-            placeholder="R$ 0,00"
-          />
-          {touched.desiredValue && errors.desiredValue && (
-            <HelperText>{errors.desiredValue}</HelperText>
-          )}
-        </Label>
-        <Button type="submit">Calcular</Button>
-      </Wrapper>
-    </Form>
+    <Formik
+      initialValues={{
+        desiredValue: 0,
+      }}
+      validate={(values: Values) => {
+        let errors: FormikErrors<Values> = {};
+        if (values.desiredValue < 300 || values.desiredValue > 10000) {
+          errors.desiredValue =
+            "O valor do empréstimo deve ser maior que R$ 300,00 e menor que R$ 10.000,00";
+        }
+        return errors;
+      }}
+      onSubmit={(values: Values, { setSubmitting }: FormikHelpers<Values>) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 500);
+      }}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        handleBlur,
+        touched,
+        errors,
+        values,
+      }) => (
+        <Form onSubmit={handleSubmit}>
+          <Title>Valor desejado</Title>
+          <Wrapper>
+            <Label>
+              <Input
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.desiredValue}
+                border={
+                  touched.desiredValue && errors.desiredValue && "1px solid red"
+                }
+                type="text"
+                name="desiredValue"
+                placeholder="R$ 0,00"
+              />
+              {touched.desiredValue && errors.desiredValue && (
+                <HelperText>{errors.desiredValue}</HelperText>
+              )}
+            </Label>
+            <Button type="submit">Calcular</Button>
+          </Wrapper>
+        </Form>
+      )}
+    </Formik>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,14 @@
 import Layout from "../components/Layout";
-import { Formik, FormikHelpers, FormikErrors } from "formik";
+
 import DesiredValueForm  from '../components/DesiredValueForm';
 
-interface Values {
-  desiredValue: number;
-}
+
 
 export default function Home() {
   return (
     <Layout title="Simulação de taxas">
       <div>
-        <Formik
-          initialValues={{
-            desiredValue: 0,
-          }}
-          validate ={
-            (values : Values) => {
-              let errors: FormikErrors<Values> = {};
-              if(values.desiredValue < 300 || values.desiredValue > 10000)  {
-                errors.desiredValue = "O valor do empréstimo deve ser maior que R$ 300,00 e menor que R$ 10.000,00"
-              }
-              return errors;
-            }
-          }
-          onSubmit={(
-            values: Values,
-            { setSubmitting }: FormikHelpers<Values>
-          ) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 500);
-          }}
-        >
-          {(props) => <DesiredValueForm {...props}/>}
-          
-        </Formik>
+        <DesiredValueForm/>
       </div>
     </Layout>
   );
