09cc809868571c0ab44181ceeeb38934fb9f7d21
Taina Conde <tainabatistaconde@gmail.com>
2021-07-06 18:51:46 -0400

    feat: Show RateTableForm depending whether desiredValue was set or not

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import Layout from "../components/Layout";
-
 import DesiredValueForm  from '../components/DesiredValueForm';
 import RateTableForm from '../components/RateTableForm';
+import { useAppSelector, useAppDispatch } from "../hooks";
 
 
 
 export default function Home() {
+  const desiredValue = useAppSelector((state) => state.solicitation.desiredValue)
   return (
     <Layout title="Simulação de taxas">
       <div>
         <DesiredValueForm/>
-        <RateTableForm/>
+        { desiredValue !== 0 && <RateTableForm/>}
+        
       </div>
     </Layout>
   );
