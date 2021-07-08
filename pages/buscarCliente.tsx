6fd22e402da1cb1dae832e7f40c458bd1b6d40d6
Taina Conde <tainabatistaconde@gmail.com>
2021-07-08 11:11:31 -0400

    feat: Create buscarCliente page

---
+++ pages/buscarCliente.tsx
@@ -0,0 +1,16 @@
+import Layout from "../components/Layout";
+import { useAppSelector, useAppDispatch } from "../hooks";
+
+
+
+export default function BuscarCliente() {
+  const solicitation = useAppSelector((state) => state.solicitation)
+  return (
+    <Layout title="Solicitar Empréstimo">
+      <div>
+        Buscar cliente
+        
+      </div>
+    </Layout>
+  );
+}
