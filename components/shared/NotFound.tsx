445f793d645f252980e064ba838ef6c293beef5f
Taina Conde <tainabatistaconde@gmail.com>
2021-07-30 15:39:50 -0400

    style: NotFound - style colors and font-sizes

--- components/shared/NotFound.tsx
+++ components/shared/NotFound.tsx
@@ -3,15 +3,15 @@ import Link from 'next/link';
 
 export default function NotFound() {
     return (<Wrapper>
-        <span>
+        <Error>
             404
-        </span>
+        </Error>
         <span>
             Não Encontrado
         </span>
         <span>
             <Link href = "/">
-                <a>voltar para a página principal</a>
+                <HomeLink>voltar para a página principal</HomeLink>
             </Link>
         </span>
         
@@ -21,13 +21,22 @@ export default function NotFound() {
 }
 
 const Wrapper = styled.div`
+color: ${props => props.theme.colors.text.main};
     font-size: 25px;
     height: 300px;
     width: 100%;
+    border-radius: ${props => props.theme.input.borderRadius};
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     background-color: ${props => props.theme.colors.background.primary};
 
+`
+const Error = styled.span`
+    font-size: 70px;
+`
+const HomeLink = styled.a`
+    color: ${props => props.theme.colors.primary.dark};
+    text-decoration: underline;
 `
