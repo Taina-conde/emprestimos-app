78c3954c384fe4e2f2679b7acabb65227903d2aa
Taina Conde <tainabatistaconde@gmail.com>
2021-07-30 17:07:49 -0400

    feat: Create confirmacao page

---
+++ pages/confirmacao/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "../../components/Layout";
+
+
+export default function ConfirmacaoPage() {
+
+  
+  return (
+    <Layout title="Detalhe de Solicitação">
+    confirmacao
+    </Layout>
+  );
+}
